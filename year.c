ee91164441ae0d1aceb660e9e4303aafeb1188c8
soyo520 <nagasakisoyo804@gmail.com>
2024-09-22 22:28:25 +0800

    Add files via upload

---
+++ year.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+    int years,days;
+    years = 18;
+    days = 365*years;
+    printf("I am %d years old,it is %d days.\n",years,days);
+    getchar();
+    return 0;
+    }
