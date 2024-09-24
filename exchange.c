e0044135dd2e8fb70d824231efe7a5a048553197
soyo520 <nagasakisoyo804@gmail.com>
2024-09-24 23:58:15 +0800

    Add files via upload

---
+++ exchange.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+    int a,b;
+    printf("交换前:");
+    scanf("a=%d,b=%d",&a,&b);
+    printf("交换后:a=%d,b=%d\n",b,a);
+    system("pause");
+    return 0;
+}
